9aa5a94a6662388a3ede39a78a574cfcf10cad48
soleraken <87080502+soleraken@users.noreply.github.com>
2021-07-08 21:47:15 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Clause and Phrases</title>
+	<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<?php
+    include("nav.php");
+?> 
+
+<div class="col-md-8" style="font-size: 18px;">
+	<img src="p.jpg" style="width: 800px; margin-left: 30px; height: 500px;">
+</div>
+<div class="col-md-4">
+	<h4 style="margin-top: 275px; border-bottom: solid black 1px; text-align: center;">Final Project in English Proficiency Program</h4>
+	<p>Submitted By: BSIT 3B Students</p>
+	<p style="text-align: center;">Members</p>
+	<table class="table">
+          <!--< thead>
+            <tr>
+                <th><center>Phrases</center> </th>
+                <th> <center>Clauses</center> </th>
+            </tr>
+          </thead> -->
+          <tbody>
+            <tr>
+                <td>Shan Carl Belgica</td>
+                <td>Michael Brigala</td>
+            </tr>
+            <tr>
+                <td>Kenneth Solera</td>
+                <td>Brent Louie Pontillas</td>
+            </tr>
+              <tr>
+                <td>Michael Bein Pili</td>
+                <td>Milbert Hallare</td>
+            </tr>
+            <tr>
+                <td>Don jay Raluta</td>
+                <td>Melody Barra</td>
+            </tr>
+          </tbody>
+      </table>
+</div>
+
+	
+		
+</div>
+
+</body>
+</html>
---
+++ nav.php
@@ -0,0 +1,17 @@
+<style type="text/css">
+  li{
+    font-size: 18px;
+  }
+</style>
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="index.php">English Proficiency</a>
+    </div>
+    <ul class="nav navbar-nav" style="float: right;">
+      <li class="active"><a href="index.php">Home</a></li>
+      <li><a href="eng1.php">Clause and Phrases</a></li>
+      <li><a href="eng2.php">Dependent and Independent</a></li>
+    </ul>
+  </div>
+</nav>
